92ac8feeabb90453efd72ddf152bbd660382e0cd
Logan Hill <lh0885887@otc.edu>
2025-04-16 08:55:29 -0500

    Initial Commit

---
+++ MoodApp/MoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppApp.swift
+//  MoodApp
+//
+//  Created by HILL, LOGAN on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
