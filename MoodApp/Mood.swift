4b38e9c1c2a7df4b48134d381feaf4ee3e3ced70
Logan Hill <lh0885887@otc.edu>
2025-04-16 09:23:53 -0500

    Enum complete

--- MoodApp/Mood.swift
+++ MoodApp/Mood.swift
@@ -38,12 +38,20 @@ enum Mood: String, CaseIterable, Identifiable {
         }
     }
     
-    var
+    var mood_description: String {
+        switch self {
+        case .happy:
+            return "You are feeling quite chipper!"
+        case .sad:
+            return "You've had better days..."
+        case .angry:
+            return "Everyone better stay out of your way."
+        case .confused:
+            return "You don't know what's going on."
+        case .sick:
+            return "You feel under the weather..."
+        case .scared:
+            return "You want to run and hide!"
+        }
+    }
 }
-
-//case happy = "You are feeling quite chipper!"
-//case sad = "You've had better days..."
-//case angry = "Everyone better stay out of your way."
-//case confused = "What's going on here???"
-//case sick = "BLEGH"
-//case scared = "AHHHHHH"
