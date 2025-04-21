da0960530154879affcd747b6d91e0e433adea40
scotty <scottistriggered@gmail.com>
2025-04-21 09:32:00 -0500

    Finished the mood picker view

--- MoodApp/MoodPicker.swift
+++ MoodApp/MoodPicker.swift
@@ -5,23 +5,56 @@
 //  Created by PATTY, SCOTT on 4/16/25.
 //
 
+// TODO: Ensure that the page dismisses correctly and updates the selected mood on the dashboard
+
 import SwiftUI
 
 struct MoodPicker: View {
-    var mood: Mood = .happy
+    @Binding var currentlySelectedMood: Mood
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        VStack {
+            Text("Select your mood!")
+                .font(.system(size: 36).bold())
+            VStack {
+                ForEach(Mood.allCases) { mood in
+                    MoodPickerElement(mood, 300)
+                    .onTapGesture {
+                        currentlySelectedMood = mood
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+/* This is the code that was inside of the for loop. I moved this out for two reasons:
+    1. Cleanliness. I like extracting repeatable code.
+    2. I had to set the preferred width for each of them using a frame.*/
+struct MoodPickerElement: View {
+    init(_ mood: Mood, _ preferredWidth: CGFloat) {
+        self.mood = mood
+        self.preferredWidth = preferredWidth;
+    }
+    // Preferred width ensures that all of the elements are the same size
+    var preferredWidth: CGFloat
+    var mood: Mood
     var body: some View {
         HStack {
             Text(mood.emoji)
-                .font(.system(size: 72))
+                .font(.system(size: 48))
                 .padding(.trailing, 16)
             Text(mood.rawValue)
-                .font(.system(size: 48).bold())
+                .font(.system(size: 24).bold())
         }.padding(16)
+            .frame(minWidth: preferredWidth)
             .background(Color.cyan.opacity(0.5))
             .cornerRadius(24)
     }
 }
 
 #Preview {
-    MoodPicker()
+    // Preview stopped working
+    //MoodPicker()
 }
