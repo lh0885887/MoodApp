fb7e88451c7efe7f4eb9a0372894012f99e024ee
Carlie-Dev <hufflepuffcarlie@gmail.com>
2025-04-21 09:18:37 -0500

    Added Icon and Mood App Description

--- MoodApp/AboutView.swift
+++ MoodApp/AboutView.swift
@@ -9,44 +9,51 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        VStack(spacing: 20){
-            VStack(spacing:10){
-                Text("About Us").font(.title).fontWeight(.bold)
-                Text("We are the Mushroom Hatted Frogs")
+        ScrollView{
+            VStack(spacing: 20){
+                Image("origami").resizable().frame(width: 100, height: 100).padding(.top, 30)
+                VStack(spacing:10){
+                    Text("About Us").font(.title).fontWeight(.bold)
+                    Text("We are the Mushroom Hatted Frogs")
+                    
+                    Text("What is The Mood App").font(.headline).fontWeight(.bold).padding(.top, 20)
+                    Text("This Mood Board is ment to help you reflect on your daily mood. Simply select your mood from our list and achieve self understanding.").padding(.leading, 30).padding(.trailing, 30)
+                    Text("Developers:").font(.headline).fontWeight(.bold).padding(.top, 20)
+                }
                 
-                Text("Developers:").font(.headline).fontWeight(.bold).padding(.top, 20)
-            }
-            
-            HStack(spacing: 30){
-                VStack(alignment: .leading, spacing: 10){
-                    
-                    HStack{
-                        Image(systemName: "person.fill")
-                        Text("Carlie Peters")
+                HStack(spacing: 30){
+                    VStack(alignment: .leading, spacing: 10){
+                        
+                        HStack{
+                            Image(systemName: "person.fill")
+                            Text("Carlie Peters")
+                        }
+                        HStack{
+                            Image(systemName: "person.fill")
+                            Text("Scott Patty")
+                        }
+                        HStack{
+                            Image(systemName: "person.fill")
+                            Text("Logan Hill")
+                        }
+                        HStack{
+                            Image(systemName: "person.fill")
+                            Text("Franklin Gainer")
+                        }
+                        
                     }
-                    HStack{
-                        Image(systemName: "person.fill")
-                        Text("Scott Patty")
+                    Divider().frame(width: 1,height : 100)
+                    VStack(alignment: .leading, spacing: 10){
+                        Text("About/Styling Lead")
+                        Text("Mood Picker Lead")
+                        Text("Enum/Data Lead")
+                        Text("Dashboard Lead")
                     }
-                    HStack{
-                        Image(systemName: "person.fill")
-                        Text("Logan Hill")
-                    }
-                    HStack{
-                        Image(systemName: "person.fill")
-                        Text("Franklin Gainer")
-                    }
-                    
                 }
-                Divider().frame(width: 1,height : 100)
-                VStack(alignment: .leading, spacing: 10){
-                    Text("About/Styling Lead")
-                    Text("Mood Picker Lead")
-                    Text("Enum/Data Lead")
-                    Text("Dashboard Lead")
-                }
+                
             }
         }
+        
             
     }
 }
